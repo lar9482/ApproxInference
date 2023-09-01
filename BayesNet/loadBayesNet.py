073ad4a65638781e4ba621236ee341b2c6eeb86a
lar9482 <lar9482@utulsa.edu>
2023-09-01 10:45:20 -0500

    Wrote function to load in a bayesian network from json files

--- BayesNet/loadBayesNet.py
+++ BayesNet/loadBayesNet.py
@@ -1,9 +1,26 @@
 from BayesNet.bayesNet import BayesNet, BayesNode
-
+import json
 """
     @param filename: string
         The file name of the json file to load in the bayes net.
 """
 def loadInBayesNet(filename):
+    rawBayesNetData = json.load(open(filename))
+    allBayesNodes = {}
+
+    for nodeId in rawBayesNetData:
+        parentIds = rawBayesNetData[nodeId]['parents']
+        cptTable = {}
+        for cptEntry in rawBayesNetData[nodeId]['prob']:
+            parentValues = cptEntry[0]
+            probability = cptEntry[1]
+            cptTable[tuple(parentValues)] = probability
+        
+        newBayesNode = BayesNode(
+            int(nodeId),
+            parentIds,
+            cptTable
+        )
+        allBayesNodes[int(nodeId)] = newBayesNode
     
-    print()
+    return BayesNet(allBayesNodes)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from BayesNet.loadBayesNet import loadInBayesNet
 def main():
     
     test = loadInBayesNet('./bn.json')
+    print()
 
 if __name__ == "__main__":
     main()
