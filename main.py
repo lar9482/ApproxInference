4eaf53c7d2c66e94b6e143b08e27057ec3486fcb
lar9482 <lar9482@utulsa.edu>
2023-09-02 14:03:50 -0500

    1. Reworked topological sorting algorithm to consider children instead 2. Beginning implementation of likelihood weighting

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from BayesNet.loadBayesNet import loadInBayesNet
 def main():
     
     test = loadInBayesNet('./Reference/polytree50.json')
-    order = test.getTopologicalOrder()
+    test.getTopologicalOrder()
     print()
 
 if __name__ == "__main__":
