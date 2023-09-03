d3c289b1fe04611bc96fbc779ee4c15a4ede9a2b
lar9482 <lar9482@utulsa.edu>
2023-09-03 15:48:03 -0500

    Initial implementation for gibbs-ask inference

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from BayesNet.loadBayesNet import loadInBayesNet
 
 def main():
     
-    BN = loadInBayesNet('./Reference/polytree50.json')
+    BN = loadInBayesNet('./Reference/polytree10.json')
 
     query = 5
     evidence = {
         0: 0,
+        1: 0,
         4: 1,
-        10: 1,
-        25: 0,
-        45: 1
+        9: 0
     }
     numSamples = 500
 
     W = BN.likelihoodWeighting_Query(query, evidence, numSamples)
+    print(W)
+    
+    G = BN.gibbsAsk_Query(query, evidence, numSamples)
+    print(G)
     
 if __name__ == "__main__":
     main()
